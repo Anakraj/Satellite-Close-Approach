07a557c35e14fc07ea67d8b7b155cc8dd2e50269
2s2e <stevendiwenshi1113@gmail.com>
2021-06-29 18:37:22 -0700

    attempted to add orekit

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Our Boeing project
 
 Goal is to create a program that can propagate orbits and detect potential collisions.
+
+All resources are listed here:
+https://www.orekit.org/static/dependency-info.html
---
+++ src/main/java/com/example/orbittracker/Main.java
@@ -0,0 +1,14 @@
+package com.example.orbittracker;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello universe");
+
+    }
+}
