5060ffe33889a8d758a9069a59b66e018239e6d5
Akash’s Laptop <akash@Akashss-MacBook-Pro.local>
2021-07-07 17:35:50 -0700

    test

--- src/main/java/com/example/orbittracker/Main.java
+++ src/main/java/com/example/orbittracker/Main.java
@@ -25,7 +25,7 @@ import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-
+        //test
         //load in the paths of the files containing tles to load in
         String[] tlePaths = {"./src/main/resources/celestrak_active.txt", "./src/main/resources/space-track_iridium.txt"};
         TLEReader tleReader = new TLEReader(tlePaths);
