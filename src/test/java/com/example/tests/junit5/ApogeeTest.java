6afe64f23225ff0d8db71d40143f1b1a2fbd242f
Akash’s Laptop <akash@Akashss-MacBook-Pro.local>
2021-07-22 11:46:30 -0700

    added apogee tests for buffers

--- src/test/java/com/example/tests/junit5/ApogeeTest.java
+++ src/test/java/com/example/tests/junit5/ApogeeTest.java
@@ -27,4 +27,22 @@ public class ApogeeTest {
     public void testapogeeTest3() {
         Assertions.assertEquals(1, Comparisons.apogeeTest(100,30,50,12, 0) ? 1 : 0);
     }
+
+    //should get true when satellite 2 is at a much larger orbit than satellite 1, but buffer is active
+    @Test
+    public void testapogeeTest4() {
+        Assertions.assertEquals(1, Comparisons.apogeeTest(100,30,200,101, 2) ? 1 : 0);
+    }
+
+    //should get truw when satellite 2 is at a much smaller orbit than satellite 1, but there is a buffer
+    @Test
+    public void testapogeeTest5() {
+        Assertions.assertEquals(1, Comparisons.apogeeTest(100,30,29,2, 2) ? 1 : 0);
+    }
+
+    //should get true when satellite 2 is at a similar orbit than satellite 1 and there is also a big buffer
+    @Test
+    public void testapogeeTest6() {
+        Assertions.assertEquals(1, Comparisons.apogeeTest(100,30,50,12, 100) ? 1 : 0);
+    }
 }
