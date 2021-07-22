be076ef68f3d7ec5eec9d496f129162c4af8ed24
Akash’s Laptop <akash@Akashss-MacBook-Pro.local>
2021-07-22 11:30:34 -0700

    fixed Apogee Test method

--- src/test/java/com/example/tests/junit5/ApogeeTest.java
+++ src/test/java/com/example/tests/junit5/ApogeeTest.java
@@ -13,18 +13,18 @@ public class ApogeeTest {
     //should get false when satellite 2 is at a much larger orbit than satellite 1
     @Test
     public void testapogeeTest1() {
-        Assertions.assertEquals(0, Comparisons.apogeeTest(30,100,101,200, 0) ? 1 : 0);
+        Assertions.assertEquals(0, Comparisons.apogeeTest(100,30,200,101, 0) ? 1 : 0);
     }
 
     //should get false when satellite 2 is at a much smaller orbit than satellite 1
     @Test
     public void testapogeeTest2() {
-        Assertions.assertEquals(0, Comparisons.apogeeTest(30,100,2,29, 0) ? 1 : 0);
+        Assertions.assertEquals(0, Comparisons.apogeeTest(100,30,29,2, 0) ? 1 : 0);
     }
 
     //should get true when satellite 2 is at a similar orbit than satellite 1
     @Test
     public void testapogeeTest3() {
-        Assertions.assertEquals(1, Comparisons.apogeeTest(30,100,12,50, 0) ? 1 : 0);
+        Assertions.assertEquals(1, Comparisons.apogeeTest(100,30,50,12, 0) ? 1 : 0);
     }
 }
