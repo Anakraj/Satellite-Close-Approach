dcf86d7f0fa33ed1a0f0b07aaf06415b6602e4c7
2s2e <stevendiwenshi1113@gmail.com>
2021-07-25 17:08:18 -0700

    Added working UI with JavaFX, modified build.gradle to use JavaFX Will now try to integrate data generation with UI

--- src/main/java/com/example/orbittracker/Main.java
+++ src/main/java/com/example/orbittracker/Main.java
@@ -1,8 +1,37 @@
 package com.example.orbittracker;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Start");
+import java.io.File;
+import java.net.URL;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        /*
+        *URL url = new File("src/main/java/ua/adeptius/goit/sample.fxml").toURI().toURL();
+Parent root = FXMLLoader.load(url);
+        * */
+        URL url = new File("src/main/resources/scene.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+
+        Scene scene = new Scene(root);
+
+        URL url2 = new File("src/main/resources/styles.css").toURI().toURL();
+        scene.getStylesheets().add(url2.toExternalForm());
+
+        stage.setTitle("JavaFX and Gradle");
+        stage.setScene(scene);
+        stage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
